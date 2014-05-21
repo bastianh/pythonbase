b5acb0ba4f5804d86f692c3e0cfec7fedb1f966f
Bastian Hoyer <dafire@gmail.com>
2014-05-21 21:53:35 +0200

    bugfix

--- redis.sh
+++ redis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-
-sysctl vm.overcommit_memory=1
+chown redis /var/lib/redis
+sysctl vm.overcommit_memory=1 > /dev/null 2>&1
 exec /sbin/setuser redis /usr/bin/redis-server /etc/redis/redis.conf > /dev/null 2>&1
